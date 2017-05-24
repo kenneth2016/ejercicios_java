43dd4f4c2194bd80f3d715840347fb7d428f70c3
kenneth2016 <kennethl1801@gmail.com>
2017-05-24 08:39:18 -0600

    ejercicio terminado

--- ejercicio java/scripts.js
+++ ejercicio java/scripts.js
@@ -1,11 +1,17 @@
 var btncrear = document.getElementById("boton")
 var barra= document.getElementById("input") 
 var container = document.getElementById("contenedor")
-var color = document.getElementById("selccion")
+var color = document.getElementById("seleccion")
 
 btncrear.onclick = function() {
 
-	for(var i = 0; i < parseINT(barra.value); i++) { resultado+="<h1>hola " + i + " </h1>"; }
+	for(var i = 0; i < (barra.value); i++) {
+		var cuadro= document.createElement("div");
+		cuadro.setAttribute("class", "squares " + color.value);
+		document.getElementById("contenedor").appendChild(cuadro);
+	}
+	 
+ }
 
-}
+
 

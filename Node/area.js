07782ff9b179f3a2425090bcc7062cc206664013
kenneth2016 <kennethl1801@gmail.com>
2017-08-07 13:35:41 -0600

    sacar areas

--- Node/area.js
+++ Node/area.js
@@ -1,24 +1,31 @@
-var figura = "rectangulo";
 
-var areaRectangulo = function(base, altura){
-	return (base * altura);
+
+forma = "triangulo"
+d1 = 10
+d2 = 15
+
+var areaTriangulo = function(d1, d2){
+        return (d1*d2)/2;
 }
 
-var areaTriangulo = function(base, altura){
-	return ((base * altura)/2);
+var areaRectangulo = function(d1, d2){
+        return d1*d2;
 }
 
-var areaCirculo = function(radio) {
-	return (radio*radio*3.14);
+var areaCirculo = function(d1){
+        return (2*3.14*(d1*d1));
 }
 
-
-var calcular = function (figura, d1, d2){
-	return(figura(d1, d2));
+var encontrar =function(forma, d1, d2){
+        return forma(d1, d2)
 }
 
-if (figura == "rectangulo"){
-	console.log(calcular(areaRectangulo))
-
+if (forma == "triangulo"){
+        console.log(encontrar(areaTriangulo, d1, d2))
 }
-
+if (forma == "rectangulo"){
+        console.log(encontrar(areaRectangulo, d1, d2))
+}
+if (forma == "circulo"){
+        console.log(encontrar(areaCirculo, d1))
+}
